6ec67aad505171af4402fb4d5379ccde0409f316
exelimpichment <oldestspy@gmail.com>
2023-03-09 16:51:29 +0100

    descriptive errors added

--- src/middleware/errorHandler.ts
+++ src/middleware/errorHandler.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import { CustomAPIError } from '../errors/customError';
+// import { CustomAPIError } from '../errors/customError';
 import { StatusCodes } from 'http-status-codes';
 
 // got it from here:
@@ -28,17 +28,31 @@ export const errorHandlerMiddleware: ErrorRequestHandler = (
     msg: err.message || 'Something went wrong try again later',
   };
   //  we have to check if some error is instance of CustomAPIError
-  if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message });
+  // if (err instanceof CustomAPIError) {
+  //   return res.status(err.statusCode).json({ msg: err.message });
+  // }
+  //^  we can comment this out since we have customError
+  //^  where we check for code and msg
+
+  if (err.name === 'ValidationError') {
+    customError.msg = Object.values(err.errors)
+      .map((item: any) => item.message)
+      //!remove any and set a proper type
+      .join(',');
+    customError.statusCode = 400;
   }
 
-  if (err.code === 11000) {
+  if (err.code && err.code === 11000) {
     customError.msg = `Duplicate value entered for ${Object.keys(
       err.keyValue
     )} field, please chose another value`;
     customError.statusCode = 400;
   }
 
-  // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err }); // < == cooment this pot late
+  if (err.name === 'CastError') {
+    customError.msg = `No item found with id: ${err.value}`;
+    customError.statusCode = 404;
+  }
+  // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err }); // < == comment this in later to see real error msgs
   return res.status(customError.statusCode).json({ msg: customError.msg });
 };
